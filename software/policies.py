8e7b8e72e2c4c275390c1404a3c5c52f88ce9f82
lucas <lucasvhspompe@gmail.com>
2017-02-25 15:54:45 +0100

    layed groundwork for RL

--- software/policies.py
+++ software/policies.py
@@ -1,17 +1,28 @@
 from simulation_enviroment import Cell
-
+import abc
 
 class BasePolicy():
+    __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
-        pass
+    @abc.abstractmethod
+    def __init__(self, reward_function):
+        self.rewards = []
+        self.reward_function = reward_function
 
+    @abc.abstractmethod
     def policy(self, s, cells):
         return [Cell.off for _ in cells]
+
+    @abc.abstractmethod
+    def reward(self, s, cells):
+        return self.reward_function(s, cells)
 
 
 
 class LatestSwitchPolicy(BasePolicy):
+
+    def __init__(self, reward_function):
+        super(LatestSwitchPolicy, self).__init__(reward_function)
 
     def policy(self, s, cells):
         cell_policies = []
@@ -21,8 +32,16 @@ class LatestSwitchPolicy(BasePolicy):
 
             elif cell.temperature <= cell.min_temp:
                 cell_policies.append(Cell.off)
-                
+
             else:
                 cell_policies.append(cell.state)
 
         return cell_policies
+
+class QLearningPolicy(BasePolicy):
+
+    def __init__(self, alpha, gamma, reward_function):
+        pass
+
+    def policy(self, s, cells):
+        pass
